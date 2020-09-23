b4235ccecd7cc5300d04f1fd4b422eceb8636d4f
ga92yud <ga92yud@mytum.de>
2020-09-23 14:38:27 +0200

    added latex source

--- gym_mujoco_planar_snake/gym_mujoco_planar_snake/agents/true_pred_plot.py
+++ gym_mujoco_planar_snake/gym_mujoco_planar_snake/agents/true_pred_plot.py
@@ -24,7 +24,9 @@ print([test_data[index, 1] for index in range(15)])
 
 assert False, "Test"'''
 
-rewards = np.concatenate((train_data, test_data))
+#rewards = np.concatenate((train_data, test_data))
+rewards = train_data
+
 
 '''obs, rew = rewards[:, 0], rewards[:, 1]
 
@@ -38,22 +40,30 @@ torch.manual_seed(0)
 
 triplet_path = "/home/andreas/Documents/pbirl-bachelorthesis/gym_mujoco_planar_snake/gym_mujoco_planar_snake/results/Mujoco-planar-snake-cars-angle-line-v1/improved_runs/vf_ensemble5_Sep_21_14:34:37/model_0"
 
+
 triplet_net = Net(27)
 triplet_net.load_state_dict(torch.load(triplet_path))
 
 #print(rewards[:, 0].shape)
 
 #assert False, "Test"
+len1 = rewards.shape[0]
+len2 = test_data.shape[0]
 
-obs = np.array([rewards[index, 0] for index in range(19900)]) #19900
-rews = np.array([rewards[index, 1] for index in range(19900)])
+obs = np.array([rewards[index, 0] for index in range(len1)]) #19900
+rews = np.array([rewards[index, 1] for index in range(len1)])
+
+obs2 = np.array([test_data[index, 0] for index in range(len2)]) #19900
+rews2 = np.array([test_data[index, 1] for index in range(len2)])
 
 #print(obs[0].shape)
 
 #assert False, "Test"
-starts = np.random.randint(0, 19900, size=500)
+starts = np.random.randint(0, len1, size=500)
 starts.sort()
 
+starts2 = np.random.randint(0, len2, size=500)
+starts2.sort()
 '''obs = obs[-500:]
 rews = rews[-500:]
 '''
@@ -61,10 +71,15 @@ rews = rews[-500:]
 obs = np.array([obs[start] for start in starts])
 rews = np.array([rews[start] for start in starts])
 
+obs2 = np.array([obs2[start] for start in starts2])
+rews2 = np.array([rews2[start] for start in starts2])
+
 inps = torch.from_numpy(obs).float()
 
+inps2 = torch.from_numpy(obs2).float()
 
 predictions = np.array([triplet_net.cum_return(inp) for inp in inps])
+predictions2 = np.array([triplet_net.cum_return(inp) for inp in inps2])
 
 print(predictions.shape)
 
@@ -73,7 +88,8 @@ print(predictions)
 
 
 #plt.plot(rews, predictions)
-plt.scatter(rews, predictions*10)
+plt.scatter(rews, predictions, color='b')
+plt.scatter(rews2, predictions2, color="r")
 
 plt.show()
 
